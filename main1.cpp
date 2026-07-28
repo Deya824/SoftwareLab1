408db17a6ae8f7f4e8f13c67f5b0b427413da40a
Fabliha Naowar Nizam Deya <deyafabliha@gmail.com>
2026-07-28 11:34:30 +0600

    ADD A New Calculation

---
+++ main1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main() {
+    int t;
+   cin>>t;
+   while(t--){
+    
+    
+     int c1;
+     cin>>c1;
+      int a[c1];
+     for( int i=0;i<c1;i++){
+        cin>>a[i];
+     } 
+   sort(a,a+c1);
+   cout<<a[c1-1]+1-a[0]<<"\n";
+
+   }
+
+    return 0;
+}
